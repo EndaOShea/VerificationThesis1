90f413fb2ef97abd86924e52e91bc97014db3082
endao3 <13062344@studentmail.ul.ie>
2018-06-15 11:21:41 +0100

    Submission Report: pow2 edited

---
+++ Code/PrefixSum/Krakatoa/PrefixSum.java
@@ -0,0 +1,102 @@
+//@+ CheckArithOverflow = no
+
+
+/*@ predicate is_Even{L}(integer x) =
+  @ x%2 == 0;
+@*/
+
+/*@ predicate is_Pow2{L}(integer x) =
+  @ (x==1||x==2||x==4||x==8||x==16||x==32);
+@*/
+
+
+public class PrefixSum {
+	int a [];
+
+	public PrefixSum(int [] array) {
+	this.a = array;
+    }
+
+      /*@ 
+			@		assigns \nothing;
+      @   behavior success:
+      @   	ensures (\forall int x y; 0<=x && x<=100 && y<=x ==> is_Even(x)
+			@																											 ==>(is_Even(y)
+			@																											 ==> is_Even(x+y)) 
+			@																											 ==> \result);
+      @*/
+    private static boolean evenSumLemma() { return true; }
+
+
+	/*@
+	  @ requires 0 <= a.length && a.length <= 1000
+	  	&&  0 <= left && left <= a.length
+	  	&&  0 <= right && right <= a.length;
+		@	assigns \nothing;
+	  @ ensures \result == (2*left - right+1);
+	  @*/
+	    private int leftMost(int left, int right) {
+		return 2*left - right + 1;
+	    }
+
+
+	/*@  		 
+	  @   requires x > 0 && is_Even(x);
+		@		assigns \nothing; 
+	  @   behavior success: 
+	  @   	ensures \result == x/2 && \result*2 == x && \result < x; 
+	  @*/
+	  private static int div2 (int x) {
+	    return x/2;
+	  }
+
+
+	/*@  
+	  @   requires x >= 0;
+		@		assigns \nothing;	  
+	  @   behavior success:
+	  @   	ensures \result > 0;		
+	  @*/
+	  private static int pow2(int x) {
+		int count = 1;
+
+		/*@ loop_invariant x >= 0 && count >= 1; 
+		  @ loop_variant x-1;
+		  @// assigns count;
+ 		@*/
+		while(x > 0){
+			count += count;
+			x--;
+		}
+		return count;		 		  
+	  }
+
+
+		
+
+	 /*@ 
+	   @   requires x > 0 && x < 33;
+		 @	 assigns \nothing;		 
+	   @   behavior success:
+	   @   	ensures \result <==> is_Pow2(x);
+	   @*/
+	 private static boolean isPow2(int x){
+
+		 /*@ loop_invariant x >= 1 && x < 33;
+		   //&& is_Pow2(\old(x)) <==> is_Pow2(x);
+		   @ loop_variant x;
+			 @*/
+			while(x%2 == 0)
+			{
+				x = div2(x);
+			}
+
+			if(x==1){
+				return true;
+				}
+
+			return false;
+	 }
+
+
+}
--- Code/PrefixSum/OpenJML/PrefixSumRec.java
+++ Code/PrefixSum/OpenJML/PrefixSumRec.java
@@ -4,39 +4,39 @@ final class PrefixSumRec {
 
     /*@ spec_public @*/ private final int[] a;  
     /*@ spec_public @*/ private static int count = 1;
+    
+    //@ invariant a.length > 0;
+    //@ invariant a != null;
        
-    //@//**accessible \inv: \singleton(a);   
     //@ axiom evenSumLemma(); //axiom means assume named predicate is true
  
     
     /*@ requires array.length>0;
 	  @ requires array!=null;
-	  @// requires isPow2(a.length);
       @ ensures a.length>0 && a!= null;
     @*/
     PrefixSumRec(int [] array) {
 	this.a = array;
     }
    
-    /*@ normal_behavior
-      @   ensures \result == (\forall int x, y; 0<=x && x<=100 && y<=x; even(x) == (even(y) == even(x+y))); //if x and y are even then so is x+y
-      @   ensures \result;
-      @   accessible \nothing;
-      @   pure helper spec_public
-      @*/
-    private static boolean evenSumLemma() { return true; }
-
-    
-    
-    /*@ normal_behavior
-    @   ensures \result == (x%2==0);
-    @   accessible \nothing;
-    @   pure helper spec_public
-    @*/
-	private static boolean even (int x) 
-	{ return x%2==0;}
-	
-	
+	    /*@ normal_behavior
+	    @   ensures \result == (\forall int x, y; 0<=x && x<=100 && y<=x; even(x) == (even(y) == even(x+y))); //if x and y are even then so is x+y
+	    @   ensures \result;
+	    @   accessible \nothing;
+	    @   pure helper spec_public
+	    @*/
+	  	private static boolean evenSumLemma() { 
+		  return true; 
+	  }
+ 
+	  /*@ normal_behavior
+	  @   ensures \result == (x%2==0);
+	  @   accessible \nothing;
+	  @   pure helper spec_public
+	  @*/
+	  	private static boolean even (int x) {
+			return x%2==0;
+	   }
     
     /*@ normal_behavior
 	  @   requires x > 0;
@@ -51,73 +51,7 @@ final class PrefixSumRec {
 	    return x/2;
 	  }
 	  
-	/*@   requires x > 0 && x < Integer.MAX_VALUE/2;
-	  @   ensures \result/2 == x;
-	  @   ensures \result == x*2;
-	  @   ensures \result > x;
-	  @   accessible \nothing;
-	  @   pure helper spec_public
-	  @*/
-	  private static int mult2 (int x) {
-	    return x*2;
-	  }
-	 
-	    
-	/*@ public normal_behavior
-	  @   requires x >= 0 && x < 5;
-	  @// ensures \result == (\product int i; 0 <= i && i < x; 2);
-	  @   ensures \result > 0 && \result < 33;
-	  @// measured_by x;
-	  @   assignable count;
-	  @   spec_public
-	  @*/
-	  private static long pow2(int x) {
-		 // return x==0? 1: 2*pow2(x-1);
-		count = 1;
-
-		// @ maintaining count > 0 && count < 33; 
-		// @ maintaining x >= 0;
-		// @ maintaining _isPow2(\old(count)) == _isPow2(count);
-		// @ decreases x;
-		while(x>0)
-		{
-		 //@ assume x!=0;	
-		 count = mult2(count);
-		 x--;
-		}
-		
-		//@ assume x==0;
-		return count;	  		 		  
-	  }
-	      
-	  //@ ensures \result == (x==1||x==2||x==4||x==8||x==16||x==32);
-	  //@ model public pure helper static boolean _isPow2(int x);
-  
-		/*@ normal_behavior
-	   @   requires x > 0 && x < 33;
-	   @   ensures \result ==> ( even(x) != (x == 1) );
-	   @   ensures \result <==> _isPow2(x);
-	   @   pure helper spec_public
-	   @*/
-	 private static boolean isPow2(int x){
-		 
-		 /*@ 		   
-		   @ maintaining x > 0 && x < 33;
-		   @ maintaining _isPow2(\old(x)) == _isPow2(x); 
-		   @ decreases x; 
-			 @*/
-			while(x%2 == 0)			{
-				x = div2(x);
-			}
-
-			if(x==1){
-				return true;
-			}
-
-			return false;
-	 }
-	 
-	 /*@
+	  /*@
 	  @ requires 0 <= a.length && a.length <= Integer.MAX_VALUE / 2;
 	  @ requires 0 <= left && left <= a.length;
 	  @ requires 0 <= right && right <= a.length;
@@ -127,7 +61,70 @@ final class PrefixSumRec {
 		return 2*left - right + 1;
 	    }
 
-	    	  
+	 
+	    //@ ensures \result == (x==1||x==2||x==4||x==8||x==16||x==32);
+		//@ model public pure helper static boolean _isPow2(int x);
+	  
+			/*@ normal_behavior
+		   @   requires x > 0 && x < 33;
+		   @   ensures \result ==> ( even(x) != (x == 1) );
+		   @   ensures \result <==> _isPow2(x);
+		   @   pure helper spec_public
+		   @*/
+		 private static boolean isPow2(int x){			 
+			 /*@ 		   
+			   @ maintaining x > 0 && x < 33;
+			   @ maintaining _isPow2(\old(x)) == _isPow2(x); 
+			   @ decreases x; 
+				 @*/
+				while(x%2 == 0)			{
+					x = div2(x);
+				}
+
+				if(x==1){
+					return true;
+				}
+
+				return false;
+		 }
+
+	
+		/*@   requires x > 0 && x < Integer.MAX_VALUE/2;
+		  @   ensures \result/2 == x;
+		  @   ensures \result == x*2;
+		  @   ensures \result > x;
+		  @   accessible \nothing;
+		  @   pure helper spec_public
+		  @*/
+		  private static int mult2 (int x) {
+		    return x*2;
+		  }
+		 		    
+		/*@ public normal_behavior
+		  @   requires x >= 0;
+		  @   ensures \result > 0 && \result < 33;
+		  @   assignable count;
+		  @   spec_public
+		  @*/
+		  private static long pow2(int x) {
+			count = 1;
+
+		   /*@ 
+			 @ maintaining x >= 0;
+			 @ maintaining _isPow2(count);
+			 @ decreases x;
+			@*/
+			while(x>0)
+			{
+			 //@ assume x!=0;
+			if(count < 33)
+			 count = mult2(count);
+			 x--;
+			}
+			
+			//@ assume x==0;
+			return count;	  		 		  
+		  }	  
 	 
 }
 
@@ -135,7 +132,7 @@ final class PrefixSumRec {
 final class Runner{
 	public static void main(String[] args)
 	{		
-		PrefixSumRec p = new PrefixSumRec(new int [5]);
-		//System.out.println(p.pow2(0));
+		PrefixSumRec p = new PrefixSumRec(new int [4]);
+		//System.out.println(p.pow2(0));4
 	}
 }
